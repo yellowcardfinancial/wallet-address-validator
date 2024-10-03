02e962481dd829872d0b4ebb3ab70320e599e797
David Kilson <itzkwaysi@gmail.com>
2024-10-03 09:19:23 +0100

    chore: add ton validator

--- src/ton_validator.js
+++ src/ton_validator.js
@@ -1,6 +1,6 @@
-var cryptoUtils = require('./crypto/utils');
+var cryptoUtils = require("./crypto/utils");
 
-const regexp = new RegExp("^(EQ|UQ)[A-Za-z0-9_-]{46,48}$")
+const regexp = new RegExp("^(EQ|UQ)[A-Za-z0-9_-]{48}$");
 
 module.exports = {
     isValidAddress: function (address) {
@@ -12,24 +12,28 @@ module.exports = {
         // Otherwise check each case
         // return this.verifyChecksum(address);
 
-				return true;
+        return true;
     },
 
-		// TODO: Figure out the checksum for ton addresses
+    // TODO: Figure out the checksum for ton addresses
     verifyChecksum: function (address) {
         // Check each case
-        address = address.replace('0x','');
+        address = address.replace("0x", "");
 
         var addressHash = cryptoUtils.keccak256(address.toLowerCase());
 
-        for (var i = 0; i < 40; i++ ) {
+        for (var i = 0; i < 40; i++) {
             // The nth letter should be uppercase if the nth digit of casemap is 1
-            if ((parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) ||
-                (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])) {
+            if (
+                (parseInt(addressHash[i], 16) > 7 &&
+                    address[i].toUpperCase() !== address[i]) ||
+                (parseInt(addressHash[i], 16) <= 7 &&
+                    address[i].toLowerCase() !== address[i])
+            ) {
                 return false;
             }
         }
 
         return true;
-    }
+    },
 };
